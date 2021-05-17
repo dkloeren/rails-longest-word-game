e070aa24cb3e31c42234ef1c6d6dc1c081f54b92
David Kloeren <d.kloeren@googlemail.com>
2021-05-17 16:50:33 +0200

    wordgame .. not finished

--- Gemfile
+++ Gemfile
@@ -22,12 +22,17 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# ???????  Added by myself ... really?????????
+gem 'open-uri'
+gem 'json'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-byebug'
 end
 
 group :development do
---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+class GamesController < ApplicationController
+  # require "open-uri"
+  # require "json"
+
+  def new
+    alphabet = ("A".."Z").to_a
+    @grid = []
+    10.times do |letter|
+      @grid << alphabet.sample
+    end
+  end
+
+  def score
+    word = params[:word].upcase
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    result_serialized = URI.open(url).read
+    @result = JSON.parse(result_serialized)
+
+    # check if word only has grid letters
+    @grid = params[:grid].split(//)
+    letters = params[:word].split(//)
+
+    valid_combination = true
+    letters.each do |x|
+      if !@grid.include?(x)
+        valid_combination = false
+      end
+    end
+
+    cookies[:username] = "David"
+    cookies[:score] = word.length
+    # binding.pry
+
+    if @result["found"]
+      @answer = "%><strong>Congratulations!</strong><%= #{word} is a valid English word!"
+    else
+      if valid_combination
+        @answer = "%>Sorry but <strong><%=#{word}%></strong><%= does not seem to be a valid English word..."
+      else
+        @answer = "Sorry but <strong>#{word}</strong> can't be build out of #{@grid.join(", ")}"
+      end
+    end
+  end
+end
